c7de3b27da85fb95f8d148fb3226717bb35a7e2e
Carlos PC <xdeus.v2@gmail.com>
2024-02-02 16:02:06 -0600

    Introduction to linked lists

---
+++ Python/linked_lists.py
@@ -0,0 +1,88 @@
+class Node:
+    data = None
+    next_node = None 
+
+    def __init__(self,data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+    
+    def __repr__(self):
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next_node
+        return '-> '.join(nodes)
+
+    def is_empty(self):
+        return self.head == None
+    
+    def insert(self,data,index):
+        if index == 0:
+            self.add(data)
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev = current 
+            next_node = current.next_node
+
+            prev.next_node = new
+            new.next_node = next_node
+
+    def size(self):
+        """
+        Counts the elements and returns the size of the list
+        Takes linear time
+        """
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next_node
+        return count
+    
+    def add(self,data):
+        """
+        Adds new Node containing data at the head of the list
+        Takes O(1) time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+    
+
+    def search(self,key):
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else: 
+                current = current.next_node
+        
+        return None
+        
+
+
+
+list = LinkedList()
