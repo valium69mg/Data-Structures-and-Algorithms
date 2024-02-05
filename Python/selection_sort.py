6741a7ac8942c9d2575c50223d44cf30193599dd
Carlos PC <xdeus.v2@gmail.com>
2024-02-05 17:35:18 -0600

    Selection sort algorith with 1000 elements

---
+++ Python/selection_sort.py
@@ -0,0 +1,20 @@
+import load 
+import time
+
+numbers = load.numbers
+
+def selection_sort(values):
+    sorted_list = []
+    for i in range(0,len(values)):
+        index_to_move = index_of_min(values)
+        sorted_list.append(values.pop(index_to_move))
+    return sorted_list
+
+def index_of_min(values):
+    min_index = 0
+    for i in range(1,len(values)):
+        if values[i] < values[min_index]:
+            min_index = i
+    return min_index
+
+print(selection_sort(numbers))
