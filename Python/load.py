09ce187922bfaa28e6de1eda1ead8064ef1b4598
Carlos PC <xdeus.v2@gmail.com>
2024-02-06 17:34:52 -0600

    file_name directory variable change

--- Python/load.py
+++ Python/load.py
@@ -1,13 +1,13 @@
 file_name = "D:/My files/Python OOP/data structure and algorithms/Python/numbers.txt"
-file_name_strings = "D:/My files/Python OOP/data structure and algorithms/Python/names.txt"
-def load_numbers(file_name):
+file_name_strings = "D:/My files/Python OOP/data structure and algorithms/Python/unsorted_names.txt"
+def load_numbers():
   numbers = []
   with open(file_name) as f:
     for line in f:
       numbers.append(int(line))
   return numbers
 
-def load_names(file_name_strings):
+def load_names():
   strings = []
   with open(file_name_strings) as f:
     for line in f:
@@ -15,6 +15,8 @@ def load_names(file_name_strings):
   return strings
 
 
-numbers = load_numbers(file_name)
-strings = load_names(file_name_strings)
+numbers = load_numbers()
+strings = load_names()
+
+
 
