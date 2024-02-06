8234e23da73e0d92cd95011ddbea665915016c76
Carlos PC <xdeus.v2@gmail.com>
2024-02-06 17:22:01 -0600

    Modified load file to add the load_names function

--- Python/load.py
+++ Python/load.py
@@ -1,4 +1,5 @@
 file_name = "D:/My files/Python OOP/data structure and algorithms/Python/numbers.txt"
+file_name_strings = "D:/My files/Python OOP/data structure and algorithms/Python/names.txt"
 def load_numbers(file_name):
   numbers = []
   with open(file_name) as f:
@@ -6,6 +7,14 @@ def load_numbers(file_name):
       numbers.append(int(line))
   return numbers
 
+def load_names(file_name_strings):
+  strings = []
+  with open(file_name_strings) as f:
+    for line in f:
+      strings.append(line.rstrip('\n'))
+  return strings
+
 
 numbers = load_numbers(file_name)
+strings = load_names(file_name_strings)
 
